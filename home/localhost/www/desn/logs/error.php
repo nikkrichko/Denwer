3a5267c5a0cafbb7885a56b3f33079a27315058b
nikkrichko <nik.krichko@ya.ru>
2014-04-10 14:27:21 +0300

    changing name, administration, contacts

--- home/localhost/www/desn/logs/error.php
+++ home/localhost/www/desn/logs/error.php
@@ -8,3 +8,4 @@
 2014-04-02	06:14:57	INFO	127.0.0.1	Joomla FAILURE: 	Имя пользователя и пароль не совпадают или у вас еще нет учетной записи на сайте
 2014-04-02	06:15:07	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
 2014-04-02	06:15:15	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2014-04-10	06:53:44	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
